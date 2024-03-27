5fe7e7b3197f84e75c63be3e551f1171b4e3a811
Walter Verburg <verburg.walt@alumni.uwlax.edu>
2024-03-27 16:06:27 -0500

    Basic dissolution and reconstitution

--- Yoda66ShellCode/dictionary_encoding.py
+++ Yoda66ShellCode/dictionary_encoding.py
@@ -7,29 +7,30 @@ from pprint import pprint
 BYTE_VALUES = 256   # Number of permutations possible within one byte
 
 class DictionaryEncoder():
-    full_word_list = ["word" + str(x) for x in range(10000)]
+    
 
     def __init__(self):
         print("init" + __name__)
+        full_word_list = ["word" + str(x) for x in range(100000)]
         
-        self.word_key = random.choices(self.full_word_list, k=BYTE_VALUES)
-        pprint(self.word_key)
+        self.word_key = random.sample(full_word_list, k=BYTE_VALUES)
+        print(len(set(self.word_key)))
 
     def encode_file(self, ciphertext_file_path):
         print("CIPHER FILE :: " + ciphertext_file_path)
         with open(ciphertext_file_path) as f:
-            lines = f.readlines()
-            pprint(lines)
-            for line in lines:
-                print(line + " == " + self.decode_line(self.encode_line(line)))
+            data = f.read()
+            pprint(data)
+            for i in range(len(data)):
+                print(str(data[i]) + " == " + self.decode_line(self.encode_line(data[i])))
 
-    def encode_line(self, line) -> str:
-        print("ENCODING :: " + line)
-        return line
+    def encode_line(self, character) -> str:
+        #print("ENCODING :: " + str(character))
+        return self.word_key[ord(character)]
     
-    def decode_line(self, line) -> str:
-        print("DECODING :: " + line)
-        return line
+    def decode_line(self, line) -> bytes:
+        #print("DECODING :: " + line)
+        return chr(self.word_key.index(line))
 
 
 
